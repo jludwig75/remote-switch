1adc154509800d33657064ea1b80620c1bac4580
Jonathan Ludwig <jr.ludwig@gmail.com>
2020-11-27 09:43:51 -0700

    Moved user paramters to non-repo file

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,17 @@
 #include "webserver.h"
 #include "filesystem.h"
 
+/* NOTE: please add this file (config.h) and define the following in it:
+const char* ssid = "SSID Name";
+const char* password = "SSID Passwod";
+const char* hostName = "Desired board host name";
+const char* otaPassword = "Desired OTA password";
+const uint8_t POWER_SWITCH_PIN = pin_to_control;
+*/
+#include "config.h" // <= This file is not part of the repo code. You must add it. See above. ^^^
 
-const char* ssid = "Caradhras";
-const char* password = "Speak friend.";
-const char* hostName = "remote-switch";
-const char* otaPassword = "Redhorn!1";
 
-
-GpioSwitch powerSwitch(LED_BUILTIN, true);
+GpioSwitch powerSwitch(POWER_SWITCH_PIN, true);
 
 
 void setup()
